82ea60a21c53d7536871577306a56f16d45ef25f
ManjeriShivatankar <mshivatankar@klebcakhanapur.com>
2025-02-04 10:45:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,69 +1,175 @@
-require("dotenv").config();
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+login form backend -->index.js
 
-const app = express();
-app.use(express.json()); //Make sure this line is present
+var express=require('express')
+const cors = require('cors');
+var app=express()
+var mongodb=require('mongodb')
+const mongoClient=mongodb.MongoClient
 
-app.use(bodyParser.json());
-app.use(cors());
+ app.use(cors()); 
+//1. establish the connection
+let dburl='mongodb+srv://Sathish:sathish3210@mongofristdb.jvmrx1b.mongodb.net/'
+app.use(express.json())
+app.get('/', async function(req,res)
 
-
-const users = []; // simulating a database
-
-app.post("/register", async (req, res)=> {
-    const { username, password } = req.body;
-
-    //Validate input
-    if (username || password) {
-        return res.status(400).json({ message: "Username and password are required"});
+{
+    const client= await mongoClient.connect(dburl)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').find().toArray()
+       res.json({
+        message:'Displaying all records',
+        user
+       })
     }
+       catch(error)
+       {
+        console.log(error);
+       }
+     
+   finally{
+    client.close()
 
-    try{
-        const hashedPassword = await bcrypt.hash(password, 10);
-        user.push({ username, password: hashedPassword });
 
-        res.json({ message: "User registered successfully!" });
+   }
+
+}) 
+
+app.post('/register', async function(req,res){
+console.log('Reqe Body',req.body);
+    const client= await mongoClient.connect(dburl)
+    try
+    {
+        const client=await mongoClient.connect(dburl);
+       let db= await client.db('Sathish')
+       let user= await db.collection('UserDetails').insertOne(req.body)
+       res.json({
+        message:'Record Inserted'
+        
+       })
     }
-    catch (error) {
-        res.status(500).json({ message: "Error hashing password",error })
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
+
+s
+   }
+
+})
+app.get('/fetch/:num', async function(req,res)
+
+{
+    const client= await mongoClient.connect(dburl)
+    const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({id:id})
+       if(user)
+       {
+       res.json({
+        message:'Record Found',
+        user
+        
+       })
     }
-});
+    else{
+        res.json({
+            message:'record not found'
+        })
+    }
+    }
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
 
-//*LOGIN USER (Generate JWT) 
-app.post("/login", async (req, res)=>{
-const { username, password } = req.body;
-const user = user.find(u => u.username === username);
 
-if (!user) return res.status(400).json({ message: "User not found!"});
+   }
 
-const isValid = await bcrypt.compare(password, user.password);
-if (!isValid) return res.status(401).json({ message: "Invalid password!" });
+}) 
+app.put('/update/:num', async function(req,res)
 
-const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "1H"})
-res.json({ token });
-});
+{
+    const client= await mongoClient.connect(dburl)
+    // const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({name:req.params.num})
+       if(user)
+       {
 
-// * AUTH MODDLEWARE (Project Router)
-function authenticateToken(res,req,next) {
-    const token = req.header("Authorization")?.split(" ")[1];
-    if (!token) return res.status(401).json({ message:"Access Denied!"});
+        let data= await db.collection('userDetails').updateOne({name:req.params.num},{$set:{name:req.body.name}})
+       res.json({
+        message:'Record updated'
+        
+        
+       })
+    }
+    else{
+        res.json({
+            message:'record not found'
+        })
+    }
+    }
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
 
-    jwt.verify( token, process.env.JWT_SECRET, (err, user)=>{
-        if (err) return res.status(403).json({ message: "Invalid Token!" });
-        req.user = user;
-        next();
-    });
-}
 
-// * PROJECTED ROUTE
-app.get("/Projected", authenticateToken, (req, res) => {
-    res.json({ message: "Welcome to the protected router!", user: req.user});
-});
+   }
 
-// * START SERVER
-const PORT  = process.env.PORT || 5000;
-app.listen(PORT,() => console.log('Server running on the port $(PORT)'));
+}) 
+app.delete('/delete/:num', async function(req,res)
+
+{
+    const client= await mongoClient.connect(dburl)
+    // const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({name:req.params.num})
+       if(user)
+       {
+
+        let data= await db.collection('userDetails').deleteOne({name:req.params.num})
+       res.json({
+        message:'Record deleted'
+        
+        
+       })
+    }
+    else{
+        res.json({
+            message:'record not found'
+        })
+    }
+    }
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
+
+
+   }
+
+}) 
+
+
+app.listen(8000, ()=>console.log('Server is running in the port 8000'))
